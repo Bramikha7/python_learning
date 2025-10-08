cdc97156c9c30aaf3f7d698107fc5fe3836b2268
Bramikha7 <bramikhabennyanu@gmail.com>
2025-10-08 14:35:52 +0530

    New file added

---
+++ 8th_oct/main.py
@@ -0,0 +1,8 @@
+def greeting(username,place,id_no):
+    #start
+    print("username: ", username)
+    print("place: ",place)
+    print("ID: ",id_no)
+    #end
+greeting("John","Chennai",100)    
+
