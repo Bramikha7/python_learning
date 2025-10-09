287cfcbf7436704acb86ab56e4c49a2a77856d0e
Bramikha7 <bramikhabennyanu@gmail.com>
2025-10-09 14:07:50 +0530

    modified an existing file and created a new file

---
+++ loops_demo.py
@@ -0,0 +1,4 @@
+n=int(input("Enter the number= "))
+a=1
+for i in range(a,n+1,1):
+    print(i)
--- variables.py
+++ variables.py
@@ -3,3 +3,10 @@ foo="hello world"
 
 print(abs(a))
 print(len(foo))
+
+def sum_of_num(n,m):
+    print("Add Two Numbers= ")
+    total=n+m
+    print(total)
+sum_of_num(10,30)    
+
