399049b11594296b4df71bef3fe10dc820b77ca0
Bramikha7 <bramikhabennyanu@gmail.com>
2025-10-14 10:03:11 +0530

    new topics added

---
+++ 14th_oct/main.py
@@ -0,0 +1,39 @@
+# traversing
+
+numbers = [10,20,30,40,50]
+
+for in in range(0,len(numbers),+1):
+    print(numbers[i])
+
+
+# empty list
+
+def add_numbers(n):
+    print(n)
+    result =[]
+    for i in range(1,n+1,+1):
+        result[i-1]=i
+        print(i)
+
+
+foo = [10,20,30,40,50,60,70,80,90,100]
+
+print(foo[1])
+print(foo[2])
+print(foo[-1])
+print(foo[-2])
+print(foo[-10])
+print(len(foo))
+(foo.append(110))
+(foo.append(120))
+print(foo)
+print(foo[11])
+
+##
+print(foo[3:7])
+print(foo[3:4])
+##
+print(foo[-7:-2])
+##
+print(foo[3:])
+print(foo[:7])        
